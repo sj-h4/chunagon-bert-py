44e0c41de5fe0e8092d7205a1a40a2673dd043c9
sj <64534668+sj-h4@users.noreply.github.com>
2022-12-30 15:05:24 +0900

    Add hidden_states

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
-from transformers import AlbertTokenizer, AlbertForPreTraining
+from transformers import AlbertTokenizer, AlbertForPreTraining, AlbertConfig
 import torch
 
 model_name: str = 'ken11/albert-base-japanese-v1'
+config = AlbertConfig.from_pretrained(model_name, output_hidden_states=True)
 tokenizer = AlbertTokenizer.from_pretrained(model_name)
-model = AlbertForPreTraining.from_pretrained(model_name)
+model = AlbertForPreTraining.from_pretrained(model_name, config=config)
 input_ids = torch.tensor(tokenizer.encode("私はまだ名前を持っていない。", add_special_tokens=True)).unsqueeze(0)
 outputs = model(input_ids)
-prediction_logits = outputs.prediction_logits
-print(outputs)
+hidden_states = outputs.hidden_states
+print(hidden_states)
