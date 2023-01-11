8753f5c4d3dae3bc2c324f129831b15d920e1db0
sj <64534668+sj-h4@users.noreply.github.com>
2023-01-11 18:58:23 +0900

    .

--- src/main.py
+++ src/main.py
@@ -1,11 +1,23 @@
 from transformers import AlbertTokenizer, AlbertForPreTraining, AlbertConfig
 import torch
+import numpy as np
 
-model_name: str = 'ken11/albert-base-japanese-v1'
-config = AlbertConfig.from_pretrained(model_name, output_hidden_states=True)
-tokenizer = AlbertTokenizer.from_pretrained(model_name)
-model = AlbertForPreTraining.from_pretrained(model_name, config=config)
-input_ids = torch.tensor(tokenizer.encode("私はまだ名前を持っていない。", add_special_tokens=True)).unsqueeze(0)
-outputs = model(input_ids)
-hidden_states = outputs.hidden_states
-print(hidden_states)
+
+def get_cos_sim(v1, v2):
+    return np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
+
+
+def get_tokens():
+    model_name: str = "ken11/albert-base-japanese-v1"
+    config = AlbertConfig.from_pretrained(model_name, output_hidden_states=True)
+    tokenizer = AlbertTokenizer.from_pretrained(model_name)
+    model = AlbertForPreTraining.from_pretrained(model_name, config=config)
+    input_ids = torch.tensor(
+        tokenizer.encode("私はまだ名前を持っていない。", add_special_tokens=True)
+    ).unsqueeze(0)
+    outputs = model(input_ids)
+    hidden_states = outputs.hidden_states
+    print(input_ids)
+
+
+get_tokens()
