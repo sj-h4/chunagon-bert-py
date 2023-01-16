c195756dc7473c70b35be5b3527418ab0873653a
sj <64534668+sj-h4@users.noreply.github.com>
2023-01-16 14:41:01 +0900

    Update

--- src/main.py
+++ src/main.py
@@ -15,21 +15,22 @@ def cos_similarity(x, y, eps=1e-8):
     ny = y / (torch.sqrt(torch.sum(y ** 2)) + eps)
     return torch.dot(nx, ny)
 
-def get_token(text: str):
+def get_tokens(text: str):
     token = tokenizer.tokenize(text)
     return token
 
 def get_last_hidden_state(text: str, token_index: int):
     input_ids = torch.tensor(
-        tokenizer.encode("私はまだ名前を持っていない。", add_special_tokens=True)
+        tokenizer.encode(text, add_special_tokens=True)
     ).unsqueeze(0)
     outputs = model(input_ids)
     hidden_states = outputs.last_hidden_state
-    print(type(hidden_states))
+    print(len(hidden_states))
+    return hidden_states[token_index]
 
-def get_sum_hidden_states(text: str, token_index: int):
+def get_word_embedding(text: str, token_index: int):
     """
-    最後の4層のhidden stateを足し合わせる
+    最後の4層のhidden stateを足し合わせたベクトルを取得する
 
     Parameters
     ----------
@@ -53,13 +54,29 @@ def get_sum_hidden_states(text: str, token_index: int):
 def main():
     texts = [
         "公園で遊んだ。",
+        "この本で述べた。",
+        "大会で優勝する、",
         "ナイフで木を切った。",
+        "飛行機で東京に行く。",
         "今日で一週間が経った。",
     ]
-    sim = cos_similarity(get_sum_hidden_states(texts[0], 1), get_sum_hidden_states(texts[1], 1))
-    dist = torch.dist(get_sum_hidden_states(texts[0], 1), get_sum_hidden_states(texts[1], 1))
-    print('cos類似度', sim)
-    print('距離', dist)
+    target_text1 = texts[3]
+    target_text2 = texts[4]
+    embeddnig1 = get_word_embedding(target_text1, 1)
+    embeddnig2 = get_word_embedding(target_text2, 2)
+    print(get_tokens(target_text1))
+    print(get_tokens(target_text2))
+    sim = cos_similarity(embeddnig1, embeddnig2)
+    dist = torch.dist(embeddnig1, embeddnig2)
+    print('cos類似度', sim.item())
+    print('距離', dist.item())
+
+    hidden_state1 = get_last_hidden_state(target_text1, 1)
+    hidden_state2 = get_last_hidden_state(target_text2, 2)
+    sim = cos_similarity(hidden_state1, hidden_state2)
+    dist = torch.dist(hidden_state1, hidden_state2)
+    print('cos類似度', sim.item())
+    print('距離', dist.item())
 
 if __name__ == "__main__":
     main()
